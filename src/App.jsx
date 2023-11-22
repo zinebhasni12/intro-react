5100db97ea1bf2b177e6a77f61e9ed31f0f1e0ed
zinebhasni12 <zinebhasni.2011@gmail.com>
2023-11-22 11:30:42 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+
+import './App.css'
+
+const App = () =>{
+  return(
+    <>
+          <nav>
+        
+
+          </nav>
+<div className='carts'>
+
+          <div className='Angelina'>
+                  <h1>Angelina jolie</h1>
+                  <div className="card"></div>
+                  <a href="#title">Title/</a>
+                    <a href="#discrition>">Discription</a>
+                  <h2> Actress</h2>
+                  
+                      <p>Angelina Jolie .... born Angelina Jolie Voight 4 June 4, 1975 
+                        is an American actress, filmmaker and humanitarian. The recipient 
+                        of numerous accolades, including an Academy Award and three Golden Globe Awards, 
+                        she has been named Hollywood's highest-paid actress multiple times.
+
+                      </p>
+            </div>
+             <div className="Olivia">
+            <h1>Olivia</h1>
+            <h2>Actress</h2>
+            <p>Sarah Caroline Sinclair CBE (née Colman; born 30 January 1974), known professionally as Olivia Colman, is an English actress. Known for her work in film and television, she has received various accolades, including an Academy Award, a British Academy Film Award, two Emmy Awards, three British Academy Television Awards, and three Golden Globe Awards</p>
+            </div>
+
+
+    <div>
+      <div className="Kristen"></div>
+      <h1>Kristen Steward</h1>
+      <h2>Actress</h2>
+      <p>Kristen Jaymes Stewart born April 9, 1990 is an American actress. She has received various accolades, including a British Academy Film Award and a César Award, in addition to nominations for an Academy Award and a Golden Globe Award.</p>
+    </div>
+    </div>
+    </>
+  )
+}
+
+    
+
+
+
+export default App
